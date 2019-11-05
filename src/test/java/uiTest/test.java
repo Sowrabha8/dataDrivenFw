177f42adf025797ef164a9bf8202ec11e397dd3f
bonoshri123 <bonoshri.das@gmail.com>
2019-11-06 00:40:27 +0530

    github Welcom

---
+++ src/test/java/uiTest/test.java
@@ -0,0 +1,17 @@
+package uiTest;
+
+import org.testng.annotations.Test;
+
+public class test {  
+
+		@Test
+		public void github(){
+		System.out.println("Welcome to github"); 
+		} 
+		}
+
+
+				
+	
+
+
